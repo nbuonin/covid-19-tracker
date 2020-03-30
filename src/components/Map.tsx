c592b97d9af224c1a33ec52f0fef93779d0b0014
Nick Buonincontri <nbuonin@gmail.com>
2020-03-29 21:37:37 -0400

    Implement hover and click handler stubs

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -11,6 +11,7 @@ import { GeoJsonLayer } from '@deck.gl/layers';
 const TOKEN = 'pk.eyJ1IjoibmIyNDEzIiwiYSI6ImNrMndkdDByczAwdnkzZ28yN2dwYjF5dWIifQ.4pPTuMIJvdboMXok3Xux-A';
 
 
+// TODO: Type GeoJSON
 const Map = (countyData: any) => {
     const [viewportState, setViewport] = useState({
             viewport: {
@@ -23,6 +24,31 @@ const Map = (countyData: any) => {
     });
     const [countyLayer, setCountyLayer] = useState();
 
+    const [countyHoverData, setCountyHoverData] = useState();
+    const onCountyHover = (info: any, event: any) => {
+        setCountyHoverData({x: info.x, y: info.y, hoveredObject: info.object});
+        /*return true;*/
+    }
+
+    const onCountyClick = (info: any, event: any) => {
+        console.log('clicking');
+    }
+    const renderTooltip = () => {
+        const {x, y, hoveredObject} = countyHoverData;
+        console.log(hoveredObject)
+        return (
+            hoveredObject && (
+                <div className={""}
+                    style={{top: y, left: x, position: 'absolute', pointerEvents: 'none'}}>
+                    <div className="arrow"/>
+                    <div className="tooltip-inner">
+                        {hoveredObject.properties.NAME}
+                    </div>
+                </div>
+            )
+        )
+    }
+
     useEffect(() => {
         fetch('/data/county.json')
         .then((response) => {
@@ -34,15 +60,20 @@ const Map = (countyData: any) => {
         .then((countyData) => {
             setCountyLayer(new GeoJsonLayer({
                 data: countyData,
+                pickable: true,
                 stroked: true,
-                filled: false,
+                filled: true,
+                getFillColor: [0, 0, 0, 10],
                 getLineColor: [0, 0, 0, 255],
-                getLineWidth: 250
+                getLineWidth: 250,
+                onHover: onCountyHover,
+                onClick: onCountyClick
             }));
         });
     }, []);
 
     return (
+        <>
         <DeckGL
             layers={[countyLayer]}
             initialViewState={viewportState.viewport}
@@ -57,6 +88,8 @@ const Map = (countyData: any) => {
                 mapStyle='mapbox://styles/mapbox/light-v9'
                 mapboxApiAccessToken={TOKEN} />
         </DeckGL>
+        {countyHoverData && renderTooltip()}
+        </>
     )
 }
 
