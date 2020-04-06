9ddc53d1f45c4e631899bef4c1f9d136a4fc3ecc
Nick Buonincontri <nbuonin@gmail.com>
2020-04-06 11:02:57 -0400

    Updates

---
+++ precache-manifest.0b2fd5af486026d39efaa657f372bba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26db0cff1325eec63c90fcda468f0400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2c9a3fd63bb3a39a703",
+    "url": "/static/css/2.5852ca77.chunk.css"
+  },
+  {
+    "revision": "3785b9cb913620f85385",
+    "url": "/static/css/main.9d42ee97.chunk.css"
+  },
+  {
+    "revision": "e2c9a3fd63bb3a39a703",
+    "url": "/static/js/2.bc27b9ee.chunk.js"
+  },
+  {
+    "revision": "c29add1f6bddeaa1af6573537998dcfa",
+    "url": "/static/js/2.bc27b9ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3785b9cb913620f85385",
+    "url": "/static/js/main.bd33b526.chunk.js"
+  },
+  {
+    "revision": "79eefbb7815170a52a15",
+    "url": "/static/js/runtime-main.ea70b20a.js"
+  }
+]);
